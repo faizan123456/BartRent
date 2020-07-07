a18878faa0f0439c6118545b3317801340777418
saqibsb <ssbsbsb160@gmail.com>
2020-07-07 14:39:36 +0500

    Mern Template

---
+++ services/passport.js
@@ -0,0 +1,39 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
+const mongoose = require("mongoose");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => done(null, user));
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+      proxy: true
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        //return null or existing user obj.
+        //We Already Have Record with the Given User.
+        done(null, existingUser);
+      } else {
+        //Don't have record in Db.create New User
+        const user = await new User({
+          googleId: profile.id
+        }).save();
+        done(null, user);
+      }
+    }
+  )
+);
