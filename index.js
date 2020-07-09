c4d52f13cff194f67a2f5607312f8e8c1a3c80e8
saqibsb <ssbsbsb160@gmail.com>
2020-07-09 21:10:18 +0500

    First V of local  Authentication using passport

--- index.js
+++ index.js
@@ -1,32 +1,93 @@
+//local auth
+const path = require("path");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const cors = require("cors");
+const errorHandler = require("errorhandler");
+
 const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const cookieSession = require("cookie-session");
 const keys = require("./config/keys");
 
-require("./models/User");
-require("./services/passport");
-require("./services/fb-passport");
+//Configure mongoose's promise to global promise
+mongoose.promise = global.Promise;
 
-mongoose.connect(keys.mongoURI);
+//Configure isProduction variable
+const isProduction = process.env.NODE_ENV === "production";
 
 const app = express();
+
+app.use(cors());
+app.use(require("morgan")("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
+  session({
+    secret: "passport-tutorial",
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false
   })
 );
+
+if (!isProduction) {
+  app.use(errorHandler());
+}
+
+//// google facebook
+// app.use(
+//   cookieSession({
+//     maxAge: 30 * 24 * 60 * 60 * 1000,
+//     keys: [keys.cookieKey]
+//   })
+// );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
+mongoose.connect(keys.mongoURI);
+mongoose.set("debug", true);
+
+// require("./models/User");
+// require("./services/passport");
+// require("./services/fb-passport");
+
+//local auth
+require("./models/Users");
+require("./services/local-passport");
+app.use(require("./routes"));
+//local auth
+
 require("./routes/authRoutes")(app);
 require("./routes/authFBRputes")(app);
-app.get("/", (req, res) => {
-  res.send({ hi: "There" });
+
+//Error handlers & middlewares
+if (!isProduction) {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err
+      }
+    });
+  });
+}
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  res.json({
+    errors: {
+      message: err.message,
+      error: {}
+    }
+  });
 });
-
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT);
+app.listen(PORT, () => console.log("Server running on http://localhost:5000/"));
---
+++ routes/auth.js
@@ -0,0 +1,46 @@
+const jwt = require("express-jwt");
+
+const getTokenFromHeaders = req => {
+  const {
+    headers: { authorization }
+  } = req;
+  // console.log(headers);
+
+  // console.log("JWT", req.headers.authorization);
+  // var auth = authorization;
+  // var value = auth.split(" ")[0];
+  // console.log("split value", value === "Token");
+  // console.log("auth", auth);
+  // console.log("split", authorization.split(" ")[0] === "Token");
+  if (authorization && authorization.split(" ")[0] === "Token") {
+    console.log("inside if ");
+    return authorization.split(" ")[1];
+  }
+  console.log("outside if ....");
+  return null;
+};
+// {
+//   if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+//       return req.headers.authorization.split(' ')[1];
+//   } else if (req.query && req.query.token) {
+//     return req.query.token;
+//   }
+//   return null;
+// }
+
+const auth = {
+  required: jwt({
+    secret: "secret",
+    userProperty: "payload",
+    getToken: getTokenFromHeaders
+  }),
+
+  optional: jwt({
+    secret: "secret",
+    userProperty: "payload",
+    getToken: getTokenFromHeaders,
+    credentialsRequired: false
+  })
+};
+
+module.exports = auth;
